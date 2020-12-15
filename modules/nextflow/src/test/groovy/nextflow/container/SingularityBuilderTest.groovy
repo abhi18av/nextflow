e8fdf051960d659717f1e5494f9029671297ebb0
Abhinav Sharma <abhi18av@gmail.com>
2020-12-15 13:32:10 +0530

    Accomodate CPU and RAM for singularity

--- modules/nextflow/src/test/groovy/nextflow/container/SingularityBuilderTest.groovy
+++ modules/nextflow/src/test/groovy/nextflow/container/SingularityBuilderTest.groovy
@@ -123,6 +123,16 @@ class SingularityBuilderTest extends Specification {
         cmd == 'set +u; env - PATH="$PATH" SINGULARITYENV_TMP="$TMP" SINGULARITYENV_TMPDIR="$TMPDIR" singularity exec ubuntu.img /bin/sh -c "cd $PWD; bwa --this --that file.fastq"'
     }
 
+
+    def 'test memory and cpuset'() {
+
+        expect:
+        new SingularityBuilder('busybox')
+                .setCpus('2')
+                .build()
+                .runCommand == 'set +u; env - PATH="$PATH" SINGULARITYENV_TMP="$TMP" SINGULARITYENV_TMPDIR="$TMPDIR" singularity exec --vm-cpu 2 busybox'
+    }
+
     @Unroll
     def 'test singularity env'() {
 
