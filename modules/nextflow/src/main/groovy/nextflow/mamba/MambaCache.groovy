256d168f2308d91ba27deb78d28dd7d250cb8a53
Abhinav Sharma <abhi18av@gmail.com>
2021-04-11 20:27:56 -0300

    Initial commit with conda clone for mamba [ci skip]

---
+++ modules/nextflow/src/main/groovy/nextflow/mamba/MambaCache.groovy
@@ -0,0 +1,343 @@
+/*
+ * Copyright 2020-2021, Seqera Labs
+ * Copyright 2013-2019, Centre for Genomic Regulation (CRG)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nextflow.mamba
+
+import groovy.transform.CompileStatic
+import groovy.transform.PackageScope
+import groovy.util.logging.Slf4j
+import groovyx.gpars.dataflow.DataflowVariable
+import groovyx.gpars.dataflow.LazyDataflowVariable
+import nextflow.Global
+
+import nextflow.file.FileMutex
+import nextflow.util.CacheHelper
+import nextflow.util.Duration
+import nextflow.util.Escape
+import org.yaml.snakeyaml.Yaml
+
+import java.nio.file.FileSystems
+import java.nio.file.NoSuchFileException
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.util.concurrent.ConcurrentHashMap
+
+/**
+ * Handle Mamba environment creation and caching
+ *
+ * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
+ */
+@Slf4j
+@CompileStatic
+class MambaCache {
+
+    /**
+     * Cache the prefix path for each Mamba environment
+     */
+    static final private Map<String, DataflowVariable<Path>> mambaPrefixPaths = new ConcurrentHashMap<>()
+
+    /**
+     * The Mamba settings defined in the nextflow config file
+     */
+    private MambaConfig config
+
+    /**
+     * Timeout after which the environment creation is aborted
+     */
+    private Duration createTimeout = Duration.of('20min')
+
+    private String createOptions
+
+    private Path configCacheDir0
+
+    @PackageScope
+    String getCreateOptions() { createOptions }
+
+    @PackageScope
+    Duration getCreateTimeout() { createTimeout }
+
+    @PackageScope
+    Map<String, String> getEnv() { System.getenv() }
+
+    @PackageScope
+    Path getConfigCacheDir0() { configCacheDir0 }
+
+    /** Only for debugging purpose - do not use */
+    @PackageScope
+    MambaCache() {}
+
+    /**
+     * Create a Mamba env cache object
+     *
+     * @param config A {@link Map} object
+     */
+    MambaCache(MambaConfig config) {
+        this.config = config
+
+        if( config.createTimeout )
+            createTimeout = config.createTimeout as Duration
+
+        if( config.createOptions )
+            createOptions = config.createOptions
+
+        if( config.cacheDir )
+            configCacheDir0 = (config.cacheDir as Path).toAbsolutePath()
+    }
+
+    /**
+     * Retrieve the directory where store the mamba environment.
+     *
+     * If tries these setting in the following order:
+     * 1) {@code mamba.cacheDir} setting in the nextflow config file;
+     * 2) the {@code $workDir/mamba} path
+     *
+     * @return
+     *      the {@code Path} where store the mamba envs
+     */
+    @PackageScope
+    Path getCacheDir() {
+
+        def cacheDir = configCacheDir0
+
+        if( !cacheDir && getEnv().NXF_CONDA_CACHEDIR )
+            cacheDir = getEnv().NXF_CONDA_CACHEDIR as Path
+
+        if( !cacheDir )
+            cacheDir = getSessionWorkDir().resolve('mamba')
+
+        if( cacheDir.fileSystem != FileSystems.default ) {
+            throw new IOException("Cannot store Mamba environments to a remote work directory -- Use a POSIX compatible work directory or specify an alternative path with the `mamba.cacheDir` config setting")
+        }
+
+        if( !cacheDir.exists() && !cacheDir.mkdirs() ) {
+            throw new IOException("Failed to create Mamba cache directory: $cacheDir -- Make sure a file with the same does not exist and you have write permission")
+        }
+
+        return cacheDir
+    }
+
+    @PackageScope
+    Path getSessionWorkDir() {
+        Global.session.workDir
+    }
+
+    @PackageScope
+    boolean isYamlFilePath(String str) {
+        (str.endsWith('.yml') || str.endsWith('.yaml')) && !str.contains('\n')
+    }
+
+    boolean isTextFilePath(String str) {
+        str.endsWith('.txt') && !str.contains('\n')
+    }
+
+
+    /**
+     * Get the path on the file system where store a Mamba environment
+     *
+     * @param mambaEnv The mamba environment
+     * @return the mamba unique prefix {@link Path} where the env is created
+     */
+    @PackageScope
+    Path mambaPrefixPath(String mambaEnv) {
+        assert mambaEnv
+
+        String content
+        String name = 'env'
+        // check if it's a YAML file
+        if( isYamlFilePath(mambaEnv) ) {
+            try {
+                final path = mambaEnv as Path
+                content = path.text
+                final yaml = (Map) new Yaml().load(content)
+                if( yaml.name )
+                    name = yaml.name
+                else
+                    name = path.baseName
+            }
+            catch (NoSuchFileException e) {
+                throw new IllegalArgumentException("Mamba environment file does not exist: $mambaEnv")
+            }
+            catch (Exception e) {
+                throw new IllegalArgumentException("Error parsing Mamba environment YAML file: $mambaEnv -- Chech the log file for details", e)
+            }
+        } else if( isTextFilePath(mambaEnv) ) {
+            try {
+                final path = mambaEnv as Path
+                content = path.text
+                name = path.baseName
+            }
+            catch (NoSuchFileException e) {
+                throw new IllegalArgumentException("Mamba environment file does not exist: $mambaEnv")
+            }
+            catch (Exception e) {
+                throw new IllegalArgumentException("Error parsing Mamba environment text file: $mambaEnv -- Chech the log file for details", e)
+            }
+        }
+        // it's interpreted as user provided prefix directory
+        else if( mambaEnv.contains('/') ) {
+            final prefix = mambaEnv as Path
+            if( !prefix.isDirectory() )
+                throw new IllegalArgumentException("Mamba prefix path does not exist or it's not a directory: $prefix")
+            if( prefix.fileSystem != FileSystems.default )
+                throw new IllegalArgumentException("Mamba prefix path must be a POSIX file path: $prefix")
+
+            return prefix
+        } else if( mambaEnv.contains('\n') ) {
+            throw new IllegalArgumentException("Invalid Mamba environment definition: $mambaEnv")
+        } else {
+            content = mambaEnv
+        }
+
+        final hash = CacheHelper.hasher(content).hash().toString()
+        getCacheDir().resolve("$name-$hash")
+    }
+
+    /**
+     * Run the mamba tool to create an environment in the file system.
+     *
+     * @param mambaEnv The mamba environment definition
+     * @return the mamba environment prefix {@link Path}
+     */
+    @PackageScope
+    Path createLocalMambaEnv(String mambaEnv) {
+        final prefixPath = mambaPrefixPath(mambaEnv)
+        if( prefixPath.isDirectory() ) {
+            log.debug "Mamba found local env for environment=$mambaEnv; path=$prefixPath"
+            return prefixPath
+        }
+
+        final file = new File("${prefixPath.parent}/.${prefixPath.name}.lock")
+        final wait = "Another Nextflow instance is creatign the Mamba environment $mambaEnv -- please wait it completes"
+        final err = "Unable to acquire exclusive lock after $createTimeout on file: $file"
+
+        final mutex = new FileMutex(target: file, timeout: createTimeout, waitMessage: wait, errorMessage: err)
+        try {
+            mutex.lock { createLocalMambaEnv0(mambaEnv, prefixPath) }
+        }
+        finally {
+            file.delete()
+        }
+
+        return prefixPath
+    }
+
+    @PackageScope
+    Path makeAbsolute(String envFile) {
+        Paths.get(envFile).toAbsolutePath()
+    }
+
+    @PackageScope
+    Path createLocalMambaEnv0(String mambaEnv, Path prefixPath) {
+
+        log.info "Creating Mamba env: $mambaEnv [cache $prefixPath]"
+
+        final opts = createOptions ? "$createOptions " : ''
+        def cmd
+        if( isYamlFilePath(mambaEnv) ) {
+            cmd = "mamba env create --prefix ${Escape.path(prefixPath)} --file ${Escape.path(makeAbsolute(mambaEnv))}"
+        } else if( isTextFilePath(mambaEnv) ) {
+            cmd = "mamba create $opts--mkdir --yes --quiet --prefix ${Escape.path(prefixPath)} --file ${Escape.path(makeAbsolute(mambaEnv))}"
+        } else {
+            cmd = "mamba create $opts--mkdir --yes --quiet --prefix ${Escape.path(prefixPath)} $mambaEnv"
+        }
+
+        try {
+            runCommand(cmd)
+            log.debug "Mamba create complete env=$mambaEnv path=$prefixPath"
+        }
+        catch (Exception e) {
+            // clean-up to avoid to keep eventually corrupted image file
+            prefixPath.delete()
+            throw e
+        }
+        return prefixPath
+    }
+
+    @PackageScope
+    int runCommand(String cmd) {
+        log.trace """Mamba create
+                     command: $cmd
+                     timeout: $createTimeout""".stripIndent()
+
+        final max = createTimeout.toMillis()
+        final builder = new ProcessBuilder(['bash', '-c', cmd])
+        final proc = builder.start()
+        final err = new StringBuilder()
+        final consumer = proc.consumeProcessErrorStream(err)
+        proc.waitForOrKill(max)
+        def status = proc.exitValue()
+        if( status != 0 ) {
+            consumer.join()
+            def msg = "Failed to create Mamba environment\n  command: $cmd\n  status : $status\n  message:\n"
+            msg += err.toString().trim().indent('    ')
+            throw new IllegalStateException(msg)
+        }
+        return status
+    }
+
+    /**
+     * Given a remote image URL returns a {@link DataflowVariable} which holds
+     * the local image path.
+     *
+     * This method synchronise multiple concurrent requests so that only one
+     * image download is actually executed.
+     *
+     * @param mambaEnv
+     *      Mamba environment string
+     * @return
+     *      The {@link DataflowVariable} which hold (and pull) the local image file
+     */
+    @PackageScope
+    DataflowVariable<Path> getLazyImagePath(String mambaEnv) {
+        if( mambaEnv in mambaPrefixPaths ) {
+            log.trace "Mamba found local environment `$mambaEnv`"
+            return mambaPrefixPaths[mambaEnv]
+        }
+
+        synchronized (mambaPrefixPaths) {
+            def result = mambaPrefixPaths[mambaEnv]
+            if( result == null ) {
+                result = new LazyDataflowVariable<Path>({ createLocalMambaEnv(mambaEnv) })
+                mambaPrefixPaths[mambaEnv] = result
+            } else {
+                log.trace "Mamba found local cache for environment `$mambaEnv` (2)"
+            }
+            return result
+        }
+    }
+
+    /**
+     * Create a mamba environment caching it in the file system.
+     *
+     * This method synchronise multiple concurrent requests so that only one
+     * environment is actually created.
+     *
+     * @param mambaEnv The mamba environment string
+     * @return the local environment path prefix {@link Path}
+     */
+    Path getCachePathFor(String mambaEnv) {
+        def promise = getLazyImagePath(mambaEnv)
+        def result = promise.getVal()
+        if( promise.isError() )
+            throw new IllegalStateException(promise.getError())
+        if( !result )
+            throw new IllegalStateException("Cannot create Mamba environment `$mambaEnv`")
+        log.trace "Mamba cache for env `$mambaEnv` path=$result"
+        return result
+    }
+
+}
---
+++ modules/nextflow/src/test/groovy/nextflow/mamba/MambaCacheTest.groovy
@@ -0,0 +1,349 @@
+/*
+ * Copyright 2020-2021, Seqera Labs
+ * Copyright 2013-2019, Centre for Genomic Regulation (CRG)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nextflow.mamba
+
+import nextflow.mamba.MambaCache
+import nextflow.mamba.MambaConfig
+import spock.lang.Specification
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
+/**
+ *
+ * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
+ */
+class MambaCacheTest extends Specification {
+
+    def 'should env file'() {
+
+        given:
+        def cache = new MambaCache()
+
+        expect:
+        !cache.isYamlFilePath('foo=1.0')
+        cache.isYamlFilePath('env.yml')
+        cache.isYamlFilePath('env.yaml')
+    }
+
+    def 'should text file'() {
+
+        given:
+        def cache = new MambaCache()
+
+        expect:
+        !cache.isTextFilePath('foo=1.0')
+        !cache.isTextFilePath('env.yaml')
+        !cache.isTextFilePath('foo.txt\nbar.txt')
+        cache.isTextFilePath('env.txt')
+        cache.isTextFilePath('foo/bar/env.txt')
+    }
+
+
+    def 'should create mamba env prefix path for a string env'() {
+
+        given:
+        def ENV = 'bwa=1.7.2'
+        def cache = Spy(MambaCache)
+        def BASE = Paths.get('/mamba/envs')
+
+        when:
+        def prefix = cache.mambaPrefixPath(ENV)
+        then:
+        1 * cache.isYamlFilePath(ENV)
+        1 * cache.getCacheDir() >> BASE
+        prefix.toString() == '/mamba/envs/env-eaeb133f4ca62c95e9c0eec7ef8d553b'
+    }
+
+
+    def 'should create mamba env prefix path for a yaml env file'() {
+
+        given:
+        def folder = Files.createTempDirectory('test')
+        def cache = Spy(MambaCache)
+        def BASE = Paths.get('/mamba/envs')
+        def ENV = folder.resolve('foo.yml')
+        ENV.text = '''
+            channels:
+              - biomamba
+              - defaults
+            dependencies:
+              # Default bismark
+              - star=2.5.4a
+              - bwa=0.7.15        
+            '''
+                .stripIndent(true)  // https://issues.apache.org/jira/browse/GROOVY-9423
+
+        when:
+        def prefix = cache.mambaPrefixPath(ENV.toString())
+        then:
+        1 * cache.isYamlFilePath(ENV.toString())
+        1 * cache.getCacheDir() >> BASE
+        prefix.toString() == '/mamba/envs/foo-9416240708c49c4e627414b46a743664'
+
+        cleanup:
+        folder?.deleteDir()
+
+    }
+
+    def 'should create mamba env prefix path for a env yaml file with name'() {
+
+        given:
+        def cache = Spy(MambaCache)
+        def BASE = Paths.get('/mamba/envs')
+        def ENV = Files.createTempFile('test', '.yml')
+        ENV.text = '''  
+            name: my-env-1.1
+            channels:
+              - biomamba
+              - defaults
+            dependencies:
+              # Default bismark
+              - star=2.5.4a
+              - bwa=0.7.15        
+            '''
+                .stripIndent(true)
+
+        when:
+        def prefix = cache.mambaPrefixPath(ENV.toString())
+        then:
+        1 * cache.isYamlFilePath(ENV.toString())
+        1 * cache.getCacheDir() >> BASE
+        prefix.toString() == '/mamba/envs/my-env-1.1-e7fafe40ca966397a2c0d9bed7181aa7'
+
+    }
+
+    def 'should create mamba env prefix path for a text env file'() {
+
+        given:
+        def folder = Files.createTempDirectory('test')
+        def cache = Spy(MambaCache)
+        def BASE = Paths.get('/mamba/envs')
+        def ENV = folder.resolve('bar.txt')
+        ENV.text = '''
+                star=2.5.4a
+                bwa=0.7.15   
+                multiqc=1.2.3
+                '''
+                .stripIndent(true)  // https://issues.apache.org/jira/browse/GROOVY-9423
+
+        when:
+        def prefix = cache.mambaPrefixPath(ENV.toString())
+        then:
+        1 * cache.isYamlFilePath(ENV.toString())
+        1 * cache.isTextFilePath(ENV.toString())
+        1 * cache.getCacheDir() >> BASE
+        prefix.toString() == '/mamba/envs/bar-8a4aa7db8ddb8ce4eb4d450d4814a437'
+
+        cleanup:
+        folder?.deleteDir()
+
+    }
+
+    def 'should return a mamba prefix directory'() {
+
+        given:
+        def cache = Spy(MambaCache)
+        def folder = Files.createTempDirectory('test')
+        def ENV = folder.toString()
+
+        when:
+        def prefix = cache.mambaPrefixPath(ENV)
+        then:
+        1 * cache.isYamlFilePath(ENV)
+        0 * cache.getCacheDir()
+        prefix.toString() == folder.toString()
+
+        cleanup:
+        folder?.deleteDir()
+
+    }
+
+
+    def 'should create a mamba environment'() {
+
+        given:
+        def ENV = 'bwa=1.1.1'
+        def PREFIX = Files.createTempDirectory('foo')
+        def cache = Spy(MambaCache)
+
+        when:
+        // the prefix directory exists ==> no mamba command is executed
+        def result = cache.createLocalMambaEnv(ENV)
+        then:
+        1 * cache.mambaPrefixPath(ENV) >> PREFIX
+        0 * cache.isYamlFilePath(ENV)
+        0 * cache.runCommand(_)
+        result == PREFIX
+
+        when:
+        PREFIX.deleteDir()
+        result = cache.createLocalMambaEnv0(ENV, PREFIX)
+        then:
+        1 * cache.isYamlFilePath(ENV)
+        0 * cache.makeAbsolute(_)
+        1 * cache.runCommand("mamba create --mkdir --yes --quiet --prefix $PREFIX $ENV") >> null
+        result == PREFIX
+
+    }
+
+    def 'should create mamba env with options'() {
+        given:
+        def ENV = 'bwa=1.1.1'
+        def PREFIX = Paths.get('/foo/bar')
+        def cache = Spy(MambaCache)
+
+        when:
+        cache.createOptions = '--this --that'
+        def result = cache.createLocalMambaEnv0(ENV, PREFIX)
+        then:
+        1 * cache.isYamlFilePath(ENV)
+        1 * cache.isTextFilePath(ENV)
+        0 * cache.makeAbsolute(_)
+        1 * cache.runCommand("mamba create --this --that --mkdir --yes --quiet --prefix $PREFIX $ENV") >> null
+        result == PREFIX
+    }
+
+
+    def 'should create a mamba env with a yaml file'() {
+
+        given:
+        def ENV = 'foo.yml'
+        def PREFIX = Paths.get('/mamba/envs/my-env')
+        def cache = Spy(MambaCache)
+
+        when:
+        def result = cache.createLocalMambaEnv0(ENV, PREFIX)
+        then:
+        1 * cache.isYamlFilePath(ENV)
+        0 * cache.isTextFilePath(ENV)
+        1 * cache.makeAbsolute(ENV) >> Paths.get('/usr/base').resolve(ENV)
+        1 * cache.runCommand("mamba env create --prefix $PREFIX --file /usr/base/foo.yml") >> null
+        result == PREFIX
+
+    }
+
+    def 'should create a mamba env with a text file'() {
+
+        given:
+        def ENV = 'foo.txt'
+        def PREFIX = Paths.get('/mamba/envs/my-env')
+        def cache = Spy(MambaCache)
+
+        when:
+        cache.createOptions = '--this --that'
+        def result = cache.createLocalMambaEnv0(ENV, PREFIX)
+        then:
+        1 * cache.isYamlFilePath(ENV)
+        1 * cache.isTextFilePath(ENV)
+        1 * cache.makeAbsolute(ENV) >> Paths.get('/usr/base').resolve(ENV)
+        1 * cache.runCommand("mamba create --this --that --mkdir --yes --quiet --prefix $PREFIX --file /usr/base/foo.txt") >> null
+        result == PREFIX
+
+    }
+
+    def 'should get options from the config'() {
+
+        when:
+        def cache = new MambaCache(new MambaConfig())
+        then:
+        cache.createTimeout.minutes == 20
+        cache.createOptions == null
+        cache.configCacheDir0 == null
+
+        when:
+        cache = new MambaCache(new MambaConfig(createTimeout: '5 min', createOptions: '--foo --bar', cacheDir: '/mamba/cache'))
+        then:
+        cache.createTimeout.minutes == 5
+        cache.createOptions == '--foo --bar'
+        cache.configCacheDir0 == Paths.get('/mamba/cache')
+    }
+
+    def 'should define cache dir from config'() {
+
+        given:
+        def folder = Files.createTempDirectory('test'); folder.deleteDir()
+        def config = new MambaConfig(cacheDir: folder.toString())
+        MambaCache cache = Spy(MambaCache, constructorArgs: [config])
+
+        when:
+        def result = cache.getCacheDir()
+        then:
+        0 * cache.getSessionWorkDir()
+        result == folder
+        result.exists()
+
+        cleanup:
+        folder?.deleteDir()
+    }
+
+    def 'should define cache dir from rel path'() {
+
+        given:
+        def folder = Paths.get('.test-mamba-cache-' + Math.random())
+        def config = new MambaConfig(cacheDir: folder.toString())
+        MambaCache cache = Spy(MambaCache, constructorArgs: [config])
+
+        when:
+        def result = cache.getCacheDir()
+        println result
+        then:
+        0 * cache.getSessionWorkDir()
+        result == folder.toAbsolutePath()
+        result.exists()
+
+        cleanup:
+        folder?.deleteDir()
+    }
+
+    def 'should define cache dir from env'() {
+
+        given:
+        def folder = Files.createTempDirectory('test'); folder.deleteDir()
+        def config = new MambaConfig()
+        MambaCache cache = Spy(MambaCache, constructorArgs: [config])
+
+        when:
+        def result = cache.getCacheDir()
+        then:
+        2 * cache.getEnv() >> [NXF_CONDA_CACHEDIR: folder.toString()]
+        0 * cache.getSessionWorkDir()
+        result == folder
+        result.exists()
+
+        cleanup:
+        folder?.deleteDir()
+    }
+
+    def 'should define cache dir from session workdir'() {
+
+        given:
+        def folder = Files.createTempDirectory('test');
+        def cache = Spy(MambaCache)
+
+        when:
+        def result = cache.getCacheDir()
+        then:
+        1 * cache.getSessionWorkDir() >> folder
+        result == folder.resolve('mamba')
+        result.exists()
+
+        cleanup:
+        folder?.deleteDir()
+    }
+}
